bafed9b26f8575f8c595223905d7f7db227fe7fd
hiroto2002 <hiroto2002h14@icloud.com>
2022-07-21 22:05:52 +0900

    commit

--- src/components/three/Effects.jsx
+++ src/components/three/Effects.jsx
@@ -2,7 +2,6 @@
  * ポストプロセッシングによるエフェクト
  */
 import { EffectComposer, Bloom} from '@react-three/postprocessing'
-import { DotScreen } from '@react-three/postprocessing'
 
 import { Suspense } from 'react'
 
@@ -11,7 +10,6 @@ export default function Effects(){
     <Suspense fallback={null}>
         <EffectComposer multisampling={8} autoclear={false}>
             <Bloom kernelSize={3} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={1.0} />
-            {/* <DotScreen angle={Math.PI * 0.25} scale={1.0}/> */}
         </EffectComposer>
     </Suspense>
     )
